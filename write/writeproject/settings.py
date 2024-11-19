70e655f2c91a1cf54c3107a8a70daf5c78add4ce
king0jin <et0709@naver.com>
2024-11-19 09:32:36 +0900

    Project설정변경

--- write/writeproject/settings.py
+++ write/writeproject/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django',
+    'rest_framework',
+    "writeapp"
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'writeproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cqrs',
+        'USER': 'root',
+        'PASSWORD': '000000',
+        'HOST': '127.0.0.1',
+        'PORT': ''
     }
 }
 
@@ -105,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 

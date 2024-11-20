b52723e0451505ec60a9d9813b4b13dbc1ccef2d
king0jin <et0709@naver.com>
2024-11-20 15:05:32 +0900

    POST컴포넌트 App.js연결

--- client/cqrsclient/src/App.js
+++ client/cqrsclient/src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import {Paper} from "@material-ui/core"
+import PostBook from "./PostBook";
+import Axios from "axios";
+
 
 function App() {
+   //데이터 추가를 위한 함수
+   const post = (book) => {
+    console.log("book : ", book);
+      Axios.post("http://127.0.0.1:8080/cqrs/book/", book).then((response) => {
+        console.log(response.data)
+        if (response.data.bid) {
+          alert("저장에 성공했습니다.")
+        } else {
+          alert("코멘트를 저장하지 못했습니다.");
+        }
+      });
+    };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Paper style={{ margin: 16 }}>
+        <PostBook post = {post}/>
+      </Paper> 
     </div>
   );
 }

1151fad585066a5c013500a1df80f967330c9826
king0jin <et0709@naver.com>
2024-11-23 23:35:06 +0900

    애플리케이션 시작 test 설정

--- read/readapp/apps.py
+++ read/readapp/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ReadappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'readapp'
+    #test : 앱을 시작하자마자 한 번만 읽기 작업 수행
+    def ready(self):
+        print("앱을 시작하였습니다.")

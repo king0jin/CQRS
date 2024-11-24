4e2db5235d0a3754e6a0a61eea718e67c8623067
king0jin <et0709@naver.com>
2024-11-24 10:59:50 +0900

    readapp url 처리

--- read/readapp/views.py
+++ read/readapp/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from pymongo import MongoClient
+from bson import json_util
+import json
 
 # Create your views here.
+@api_view(['GET'])
+def bookAPI(request):
+    # 데이터베이스 연결
+    conn = MongoClient('127.0.0.1')
+    # 데이터베이스 설정
+    db = conn.cqrs
+    # 컬렉션 설정
+    collect = db.books
+    # 전체 문서 조회
+    result = collect.find()
+    # 조회 문서 출력
+    data = []
+    for r in result :
+        print(type(r))
+        data.append(r)
+    return Response(json.loads(json_util.dumps(data)), status=status.HTTP_201_CREATED)
